f39100a14fde13e466f4e21178a127748ef1ea36
Alfeu Valente <79662208+FeuValente@users.noreply.github.com>
2022-05-11 12:31:17 -0300

    Create area-do-circulo.js

---
+++ JavaScript/area-do-circulo.js
@@ -0,0 +1,7 @@
+// a função gets é implementada dentro do sistema para ler as entradas(inputs) dos dados e a função print para imprimir a saída (output) de dados e já pula uma linha ("\n")
+// Abaixo segue um exemplo de código que você pode ou não utilizar
+
+var TT = 3.14159;
+var raio = parseFloat(gets());
+var area =  TT *  Math.pow(raio,2)
+print("A=" + area.toFixed(4));
